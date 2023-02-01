11d2b63dc8dfffc07e2838cb9364ddb67399020b
QueAnka <annika.nylin@hotmail.com>
2023-02-01 19:05:20 +0100

    input exercise, solution

---
+++ input-exercise/script.js
@@ -0,0 +1,9 @@
+let toUpperCaseInput= document.querySelector('#upper-case-input')
+
+
+
+toUpperCaseInput.addEventListener('input', () =>{
+	
+toUpperCaseInput.value=toUpperCaseInput.value.toUpperCase()
+
+})
